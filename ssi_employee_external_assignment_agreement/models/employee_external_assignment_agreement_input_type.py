b1a5c12b64906f4ec6e198fc20407468de36036c
Andhitia Rama <andhitia.r@gmail.com>
2026-05-15 22:12:04 +0700

    [UPD] ssi_employee_external_assignment_agreement
    * Add default_amount field to employee_external_assignment_agreement_input_type
    * Add onchange_amount on employee_external_assignment_agreement_input based on input_type_id
    * Update unit tests to cover default_amount and onchange behavior

--- ssi_employee_external_assignment_agreement/models/employee_external_assignment_agreement_input_type.py
+++ ssi_employee_external_assignment_agreement/models/employee_external_assignment_agreement_input_type.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=R0903
 
-from odoo import models
+from odoo import fields, models
 
 
 class EmployeeExternalAssignmentAgreementInputType(models.Model):
@@ -12,3 +12,10 @@ class EmployeeExternalAssignmentAgreementInputType(models.Model):
         "mixin.master_data",
     ]
     _description = "Employee External Assignment Agreement Input Type"
+
+    default_amount = fields.Float(
+        string="Default Amount",
+        required=False,
+        default=0.0,
+        help="Default amount to be set when this input type is selected.",
+    )
