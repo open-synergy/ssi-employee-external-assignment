383eea83b2ff8804ce2c74dc96ea01037ae72c9b
Andhitia Rama <andhitia.r@gmail.com>
2026-05-15 23:04:53 +0700

    [UPD] ssi_employee_external_assignment_agreement_operating_unit
    * Tambah dependensi ssi_financial_accounting_operating_unit
    * Override operating_unit_id pada payment_term agar default dari agreement
    * Override _prepare_invoice_data untuk menyertakan operating_unit_id pada invoice
    * Tambah wizard override _prepare_payment_term_vals untuk menyertakan operating_unit_id saat membuat payment term
    * Update unit test: tambah skenario payment term operating unit

--- ssi_employee_external_assignment_agreement_operating_unit/models/employee_external_assignment_agreement_payment_term.py
+++ ssi_employee_external_assignment_agreement_operating_unit/models/employee_external_assignment_agreement_payment_term.py
@@ -2,14 +2,34 @@
 # Copyright 2025 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, fields, models
 
 
-class EmployeeExternalAssignmentAgreementPaymentTerm(
-    models.Model
-):  # pylint: disable=too-few-public-methods
+class EmployeeExternalAssignmentAgreementPaymentTerm(models.Model):
     _name = "employee_external_assignment_agreement.payment_term"
     _inherit = [
         "employee_external_assignment_agreement.payment_term",
         "mixin.single_operating_unit",
     ]
+
+    operating_unit_id = fields.Many2one(
+        comodel_name="operating.unit",
+        compute="_compute_operating_unit_id",
+        store=True,
+        readonly=False,
+        help="Operating unit for this payment term. Defaults from the agreement.",
+    )
+
+    @api.depends("agreement_id", "agreement_id.operating_unit_id")
+    def _compute_operating_unit_id(self):
+        for record in self:
+            if record.agreement_id and record.agreement_id.operating_unit_id:
+                record.operating_unit_id = record.agreement_id.operating_unit_id
+            else:
+                record.operating_unit_id = record.operating_unit_id
+
+    def _prepare_invoice_data(self):
+        result = super()._prepare_invoice_data()
+        if self.operating_unit_id:
+            result["operating_unit_id"] = self.operating_unit_id.id
+        return result
